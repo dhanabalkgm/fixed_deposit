fc8762900e82c1e39f42cc0ba6ba01956b0bf406
Dhanabal <vel.dhanabal@gmail.com>
2016-11-26 19:55:33 +0530

    added currency in locales, splitted deposit and depositer index into partial, removed fucking idea

--- lib/fd/fd_util.rb
+++ lib/fd/fd_util.rb
@@ -2,4 +2,13 @@ module FdUtil
   def fd_date(date, format= :standard)
     date.present? ? I18n.l(date, format: format) : ''
   end
+
+  def to_currency(number)
+    number_to_currency(number)
+  end
+
+  def to_currency_without_precission(number)
+    number_to_currency(number, precision: 0)
+  end
+
 end
